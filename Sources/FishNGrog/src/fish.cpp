49f8ef7c39268990342662bd2daffc5efc4d8610
valentin.clergue <valentin.clergue@etu.uca.fr>
2023-06-07 11:15:43 +0200

    fix build

--- Sources/FishNGrog/src/fish.cpp
+++ Sources/FishNGrog/src/fish.cpp
@@ -1,10 +1,11 @@
 #include "fish.h"
 
-Fish::Fish(const string& name, const bool& isTrophy, const int& price,
-           const Type& type, const Quality& quality,
-           const Cookness& cookness, const Bait& bait,
-           const Location& location)
-    : m_name(name),
+Fish::Fish(QString& name, bool& isTrophy, int& price,
+           Type& type, Quality& quality, Cookness& cookness,
+           Bait& bait, Location& location,
+           QObject *parent)     // ??
+    : QObject(parent),
+      m_name(name),
       m_isTrophy(isTrophy),
       m_price(price),
       m_type(type),
@@ -13,3 +14,92 @@ Fish::Fish(const string& name, const bool& isTrophy, const int& price,
       m_bait(bait),
       m_location(location)
 { }
+
+QString Fish::name() const
+{
+    return m_name;
+}
+
+bool Fish::isTrophy() const {
+    return m_isTrophy;
+}
+
+int Fish::price() const {
+    return m_price;
+}
+
+Type Fish::type() const {
+    return m_type;
+}
+
+Quality Fish::quality() const {
+    return m_quality;
+}
+
+Cookness Fish::cookness() const {
+    return m_cookness;
+}
+
+Bait Fish::bait() const {
+    return m_bait;
+}
+
+Location Fish::location() const {
+    return m_location;
+}
+
+void Fish::setName(QString name) {
+    if (m_name == name)
+        return;
+    m_name = name;
+    emit nameChanged(m_name);
+}
+
+void Fish::setIsTrophy(bool isTrophy) {
+    if (m_isTrophy == isTrophy)
+        return;
+    m_isTrophy = isTrophy;
+    emit isTrophyChanged(m_isTrophy);
+}
+
+void Fish::setPrice(int price) {
+    if (m_price == price)
+        return;
+    m_price = price;
+    emit isTrophyChanged(m_price);
+}
+
+void Fish::setType(Type type) {
+    if (m_type == type)
+        return;
+    m_type = type;
+    emit typeChanged(m_type);
+}
+
+void Fish::setQuality(Quality quality) {
+    if (m_quality == quality)
+        return;
+    m_quality = quality;
+    emit qualityChanged(quality);
+}
+
+void Fish::setCookness(Cookness cookness) {
+    if (m_cookness == cookness)
+        return;
+    m_cookness = cookness;
+    emit cooknessChanged(m_cookness);
+}
+
+void Fish::setBait(Bait bait) {
+    if (m_bait == bait)
+        return;
+    m_bait = bait;
+    emit baitChanged(m_bait);
+}
+
+void Fish::setLocation(Location location) {
+    if (m_location == location)
+        return;
+    m_location = location;
+    emit locationChanged(m_location);
+}
--- Sources/FishNGrog/src/fish.h
+++ Sources/FishNGrog/src/fish.h
@@ -1,7 +1,7 @@
 #ifndef FISH_H
 #define FISH_H
 
-#include <iostream>
+#include <QObject>
 
 using namespace std;
 
@@ -13,10 +13,20 @@ enum Bait { None, Leeches, Earthworms, Grubs };
 enum Location { Everywhere, Lakes, Isles_Islands, South, The_Shores_of_Plenty,
                 The_Wilds, The_Devils_Roar, Forteress, Shipwrecks, Storms};
 
-class Fish
+class Fish : public QObject
 {
-private:
-    string m_name;
+    Q_OBJECT
+
+    Q_PROPERTY(QString name         READ name       WRITE setName       NOTIFY nameChanged)
+    Q_PROPERTY(bool isTrophy        READ isTrophy   WRITE setIsTrophy   NOTIFY isTrophyChanged)
+    Q_PROPERTY(int price            READ price      WRITE setPrice      NOTIFY priceChanged)
+    Q_PROPERTY(Type type            READ type       WRITE setType       NOTIFY typeChanged)
+    Q_PROPERTY(Quality quality      READ quality    WRITE setQuality    NOTIFY qualityChanged)
+    Q_PROPERTY(Cookness cookness    READ cookness   WRITE setCookness   NOTIFY cooknessChanged)
+    Q_PROPERTY(Bait bait            READ bait       WRITE setBait       NOTIFY baitChanged)
+    Q_PROPERTY(Location location    READ location   WRITE setLocation   NOTIFY locationChanged)
+
+    QString m_name;
     bool m_isTrophy;
     int m_price;
     Type m_type;
@@ -25,9 +35,36 @@ private:
     Bait m_bait;
     Location m_location;
 public:
-    Fish(const string& name, const bool& isTrophy, const int& price,
-         const Type& type, const Quality& quality, const Cookness& cookness,
-         const Bait& bait, const Location& location);
+    explicit Fish(QString& name, bool& isTrophy, int& price,
+                  Type& type, Quality& quality, Cookness& cookness,
+                  Bait& bait, Location& location,
+                  QObject *parent = nullptr);   // ??
+    QString name() const;
+    bool isTrophy() const;
+    int price() const;
+    Type type() const;
+    Quality quality() const;
+    Cookness cookness() const;
+    Bait bait() const;
+    Location location() const;
+public slots:
+    void setName(QString name);
+    void setIsTrophy(bool isTrophy);
+    void setPrice(int price);
+    void setType(Type type);
+    void setQuality(Quality quality);
+    void setCookness(Cookness cookness);
+    void setBait(Bait bait);
+    void setLocation(Location location);
+signals:
+    void nameChanged(QString name);
+    void isTrophyChanged(bool isTrophy);
+    void priceChanged(int price);
+    void typeChanged(Type type);
+    void qualityChanged(Quality quality);
+    void cooknessChanged(Cookness cookness);
+    void baitChanged(Bait bait);
+    void locationChanged(Location location);
 };
 
 #endif // FISH_H
